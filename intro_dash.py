63d87149b551683bf5f9d9e8e8e12a371559106d
fd0livar <fdolivar@gmail.com>
2019-11-20 18:50:32 +0100

    this code is working like in the video https://www.youtube.com/watch?v=J_Cy_QjG6NE

--- intro_dash.py
+++ intro_dash.py
@@ -6,8 +6,17 @@ app = dash.Dash()
 
 app.layout = html.Div(children=[
     html.H1('Dash Tutorials'),
-    dcc.Gr
-])
+    dcc.Graph(id='example',
+                figure={
+                    'data':[
+                        {'x':[1,2,3,4,5],'y':[1,4,9,16,25], 'type':'line', 'name':'boats'},
+                        {'x':[1,2,3,4,5],'y':[3,7,23,5,65], 'type':'bar', 'name':'cars'},
+                        ],
+                    'layout':{
+                        'title':'Basic Dash Example'
+                        }
+                })
+    ])
 
 if __name__ == '__main__':
     app.run_server(debug=True)
