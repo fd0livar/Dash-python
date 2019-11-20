f000f2da240c5bdafb61a709f7cd8262f81225f0
fd0livar <fdolivar@gmail.com>
2019-11-20 13:24:31 +0100

    typo

--- ex_multiselect_bessel.py
+++ ex_multiselect_bessel.py
@@ -3,7 +3,7 @@
 import numpy as np
 import plotly as py
 import plotly.graph_objs as go
-import ipywidgets as wigdets
+import ipywidgets as widgets
 from scipy import special
 
 #py.offline.init_notebook_mode(connected=True)
@@ -32,6 +32,6 @@ def update_plot(signals, freq):
     fig = go.Figure(data=data, layout=layout1)
     py.offline.iplot(fig)
 
-signals = wigdets.SelectMultiple(options=list(range(6)), value=(0,), description='Bessel Order')
+signals = widgets.SelectMultiple(options=list(range(6)), value=(0,), description='Bessel Order')
 freq = widgets.FloatSlider(min=1, max=20, value=1, description='Freq')
 widgets.interactive(update_plot, signals=signals, freq=freq)
